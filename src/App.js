e8b9e85c99fbb031315a61426a45f13a89826275
Jensen <cwuj@M-C02T12W8GTDX.nordstrom.net>
2018-05-15 14:18:13 -0700

    added search and listing

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ class App extends Component {
       <Router>
         <Route exact path="/" component={ props =>
           <div>
+
             <nav>
               <i className="material-icons">stars</i>
               <i className="material-icons">menu</i>
             </nav>
-            <div id="splash-wrapper">
+
+            <div className="splash-wrapper">
               <img src={require("./images/hotelroom-bw-1.png")}
                 className="full-width-img"
                 alt="hotel room"/>
@@ -22,8 +24,26 @@ class App extends Component {
                 <button className="sign-up-button">Sign Up</button>
                 <p className="log-in-button pt-1"><a href="#" className="white">Log in</a></p>
               </div>
-
             </div>
+
+            <div className="search-wrapper">
+              <input type="text"
+                placeholder="Search"
+                className="wide"></input>
+              <div className="listings-wrapper pt-2">
+                <div className="listing">
+                  <img src="http://www.facetheforce.today/bb8"
+                    className="listing-thumbnail"
+                    alt="listing image"/>
+                  <div>
+                    <p className="listing-address">This is the address</p>
+                    <p className="listing-description">Semiotics messenger bag kinfolk thundercats subway tile fixie mlkshk, waistcoat blue bottle cliche shabby chic taxidermy. Put a bird on it portland keytar blue bottle, food truck franzen chicharrones blog taiyaki kale chips.</p>
+                    <p><a href="#">View Listing</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         }/>
       </Router>
